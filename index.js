6902d3040197086ce83bd9a8c7f19fb51f8e4655
Takumi <nase49946@gmail.com>
2021-12-11 15:51:14 +0900

    toggle

--- index.js
+++ index.js
@@ -4,24 +4,54 @@ const canvas = document.getElementById('canvas');
  */
 const ctx = canvas.getContext('2d');
 
+let requestId = null;
 const calculateRadian = (degrees) => (Math.PI / 180) * degrees;
 
-let x = 0;
-let y = ctx.canvas.height;
-const speed = 2;
-
-const draw = () => {
-  if (x === ctx.canvas.width) return;
-
-  window.requestAnimationFrame(draw);
-
-  ctx.beginPath();
-  ctx.arc(x, y, 30, calculateRadian(0), calculateRadian(360));
-  ctx.fill();
-  ctx.closePath();
-
-  x += speed;
-  y -= speed;
+const ball = {
+  x: 0,
+  y: ctx.canvas.height,
+  r: 30,
+  speed: 2,
+  isAnimated: false,
+  drawBall() {
+    ctx.beginPath();
+    ctx.fillStyle = 'red';
+    ctx.arc(this.x, this.y, this.r, calculateRadian(0), calculateRadian(360));
+    ctx.fill();
+    ctx.closePath();
+  },
 };
 
-draw();
+const draw = () => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  if (ball.x >= ctx.canvas.width + ball.r / 2) {
+    ball.x = 0;
+    ball.y = ctx.canvas.height;
+  }
+
+  ball.drawBall();
+
+  ball.x += ball.speed;
+  ball.y -= ball.speed;
+};
+
+const animateCanvas = () => {
+  draw();
+  requestId = window.requestAnimationFrame(animateCanvas);
+};
+
+window.onload = () => {
+  ball.isAnimated = true;
+  animateCanvas();
+};
+
+canvas.addEventListener('click', () => {
+  if (ball.isAnimated) {
+    window.cancelAnimationFrame(requestId);
+    ball.isAnimated = false;
+  } else {
+    requestId = window.requestAnimationFrame(animateCanvas);
+    ball.isAnimated = true;
+  }
+});
