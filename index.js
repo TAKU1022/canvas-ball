43b33272159ac243b30fb976924c53bb6b6777a1
Takumi <nase49946@gmail.com>
2021-12-14 19:24:37 +0900

    i

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ class Ball {
      * @type {CanvasRenderingContext2D}
      */
     this.context = context;
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
     this.width = 40;
     this.height = 40;
+    this.x = canvas.width / 2 - this.width / 2;
+    this.y = canvas.height / 2 - this.height / 2;
     this.speed = 2;
     this.isAnimated = false;
+    this.rotate = 0;
+    this.isRotate = false;
 
     this.drawRect();
   }
@@ -29,12 +31,18 @@ class Ball {
 
   drawRect() {
     this.clear();
+    this.context.save();
+    this.context.translate(this.canvas.width / 2, this.canvas.height / 2);
+    this.context.rotate(calculateRadian(this.rotate));
+    this.context.translate(-this.canvas.width / 2, -this.canvas.height / 2);
     this.context.fillStyle = 'red';
     this.context.fillRect(this.x, this.y, this.width, this.height);
+    this.context.restore();
   }
 
   move() {
     this.drawRect();
+    this.toggleRotate();
     this.x += this.speed;
   }
 
@@ -45,8 +53,12 @@ class Ball {
   reset() {
     if (this.x >= this.canvas.width) {
       this.x = 0;
-    } else {
-      return;
+    }
+  }
+
+  toggleRotate() {
+    if (this.isRotate) {
+      this.rotate += 2;
     }
   }
 }
@@ -66,6 +78,27 @@ const animate = () => {
 
 canvas.addEventListener('click', () => {
   ball.isAnimated = !ball.isAnimated;
+});
+
+const translateButton = document.getElementById('translate-button');
+const rotateButton = document.getElementById('rotate-button');
+const scaleButton = document.getElementById('scale-button');
+
+translateButton.addEventListener('click', () => {
+  const translateArray = [10, 20, 30, 40, 50, -10, -20, -30, -40, -50];
+  const randomTranslateNumber = Math.floor(
+    Math.random() * translateArray.length
+  );
+  ball.y += translateArray[randomTranslateNumber];
+});
+
+rotateButton.addEventListener('click', () => {
+  ball.isRotate = !ball.isRotate;
+});
+
+scaleButton.addEventListener('click', () => {
+  ball.width += 8;
+  ball.height += 8;
 });
 
 animate();
