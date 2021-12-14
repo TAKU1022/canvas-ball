cdea2682c982dc362734eb13d2574d3ff550803e
Takumi <nase49946@gmail.com>
2021-12-14 16:48:31 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -3,84 +3,69 @@ const canvas = document.getElementById('canvas');
  * @type {CanvasRenderingContext2D}
  */
 const context = canvas.getContext('2d');
-const width = context.canvas.width;
-const height = context.canvas.height;
 
-let requestId = null;
 const calculateRadian = (degrees) => (Math.PI / 180) * degrees;
 
-const ball = {
-  x: width / 2,
-  y: height / 2,
-  r: 30,
-  speed: 1,
-  isAnimated: false,
-  drawBall() {
-    context.beginPath();
-    context.fillStyle = 'red';
-    context.arc(
-      this.x,
-      this.y,
-      this.r,
-      calculateRadian(0),
-      calculateRadian(360)
-    );
-    context.fill();
-    context.closePath();
-  },
-};
+class Ball {
+  constructor({ canvas, context }) {
+    this.canvas = canvas;
+    /**
+     * @type {CanvasRenderingContext2D}
+     */
+    this.context = context;
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.width = 40;
+    this.height = 40;
+    this.speed = 2;
+    this.isAnimated = false;
 
-const draw = () => {
-  context.clearRect(0, 0, width, height);
-
-  if (ball.x >= width) {
-    ball.x = 0;
+    this.drawRect();
   }
 
-  ball.drawBall();
+  clear() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 
-  ball.x += ball.speed;
-};
+  drawRect() {
+    this.clear();
+    this.context.fillStyle = 'red';
+    this.context.fillRect(this.x, this.y, this.width, this.height);
+  }
 
-window.onload = () => {
-  draw();
-};
+  move() {
+    this.drawRect();
+    this.x += this.speed;
+  }
+
+  pause() {
+    this.drawRect();
+  }
+
+  reset() {
+    if (this.x >= this.canvas.width) {
+      this.x = 0;
+    } else {
+      return;
+    }
+  }
+}
+
+const ball = new Ball({ canvas, context });
 
 const animate = () => {
-  draw();
-  requestId = window.requestAnimationFrame(animate);
+  if (ball.isAnimated) {
+    ball.move();
+    ball.reset();
+  } else {
+    ball.pause();
+  }
+
+  window.requestAnimationFrame(animate);
 };
 
 canvas.addEventListener('click', () => {
-  if (ball.isAnimated) {
-    window.cancelAnimationFrame(requestId);
-    ball.isAnimated = false;
-  } else {
-    animate();
-    ball.isAnimated = true;
-  }
+  ball.isAnimated = !ball.isAnimated;
 });
 
-const translateButton = document.getElementById('translate-button');
-const rotateButton = document.getElementById('rotate-button');
-const scaleButton = document.getElementById('scale-button');
-
-translateButton.addEventListener('click', () => {
-  const translateArray = [10, 20, 30, 40, 50, -10, -20, -30, -40, -50];
-  const randomTranslateNumber = Math.floor(
-    Math.random() * translateArray.length
-  );
-  context.translate(0, translateArray[randomTranslateNumber]);
-});
-
-rotateButton.addEventListener('click', () => {
-  context.translate(width / 2, height / 2);
-  context.rotate(calculateRadian(90));
-  context.translate(-width / 2, -height / 2);
-});
-
-scaleButton.addEventListener('click', () => {
-  context.translate(width / 2, height / 2);
-  context.scale(1.1, 1.1);
-  context.translate(-width / 2, -height / 2);
-});
+animate();
